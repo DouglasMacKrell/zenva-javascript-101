5f1a86b842601fe1a111e59f09b4a1d420b531a7
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-08-13 12:17:38 -0400

    Arrays complete

---
+++ arrays.js
@@ -0,0 +1,10 @@
+let shoppingList = ["Butter", "Milk", "Eggs"];
+console.log(shoppingList);
+
+var eggs = shoppingList[2];
+shoppingList[1] = "Cheese";
+
+let listLength = shoppingList.length;
+shoppingList.push("Milk");
+
+let milk = shoppingList.pop;
