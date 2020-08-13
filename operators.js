03594d2c747c51ed54a6694ea3de0eff0373bb6f
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-08-13 12:07:04 -0400

    Operators complete

---
+++ operators.js
@@ -0,0 +1,16 @@
+// Assignment: =
+let age = 27;
+let firstName = "Douglas"
+let money = 50.05;
+
+// Arithmetic: + - * / %
+age = age + 1;
+age += 1;
+let fullName = firstName + " MacKrell";
+
+// Comparison: === == != > >= < <=
+let canAfford = money >= 20;
+
+// Logical: || && !
+let cannotAfford = !canAfford;
+let canBuyTicket = canAfford && age >= 18;
