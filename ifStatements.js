4e6ebf6259356c48b66661a90e2e3af19d881e92
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-08-14 16:25:45 -0400

    ifStatements complete

---
+++ ifStatements.js
@@ -0,0 +1,9 @@
+let lightState = "green";
+
+if (lightState === "green") {
+  console.log("Go!");
+} else if (lightState === "red") {
+  console.log("Stop!");
+} else {
+  console.log("Slow down!");
+}
